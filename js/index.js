2e7dde76c8b67db5de5bddfe5ae9c535f8bd6b52
Daniel Fieker <danielmfieker@gmail.com>
2020-01-14 17:57:52 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // Your code goes here
+const Logo = document.querySelector('.logo-heading');
+Logo.addEventListener('mouseover', () => {
+    Logo.textContent = 'Is Great Fun!';
+    Logo.style.transform = 'scale(1.5)';
+    
+})
+Logo.addEventListener('mouseleave', () => {
+    Logo.textContent = 'Fun Bus';
+    Logo.style.transform = 'scale(1)';
+})
+
+const intro = document.querySelector('.intro');
+intro.addEventListener('mouseenter', () => {
+    intro.style.backgroundColor = 'yellow';
+})
+intro.addEventListener('mouseleave', () => {
+    intro.style.backgroundColor = 'white';
+})
+
+const click = document.querySelector('.content-destination');
+click.addEventListener('dblclick', () => {
+    click.style.fontFamily = 'cursive';
+})
+
+const mousedown = document.querySelectorAll('img');
+mousedown[3].addEventListener('mousedown', () => {
+    mousedown[3].src = 'img/beach.jpg';
+})
+
+const keydown = document.querySelector('.text-content');
+window.addEventListener('keydown', () => {
+    keydown.style.color = 'blue';
+})
+
+const resize = document.querySelectorAll('img');
+window.addEventListener('resize', () => {
+    resize[0].style.width = '50%';
+})
+
+const destination = document.querySelectorAll('.destination');
+window.addEventListener('scroll', () => {
+    destination.style.backgroundColor = 'gold';
+})
